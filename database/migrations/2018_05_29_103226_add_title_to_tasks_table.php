a9d9694eeba00e4e4db461a7a4a56c281bf0e3de
marienaomi.iwata <marienaomi.iwata@rakuten.com>
2018-05-29 03:42:07 +0000

    add

--- database/migrations/2018_05_29_103226_add_title_to_tasks_table.php
+++ database/migrations/2018_05_29_103226_add_title_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddTitleToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('title, 10');
+            $table->string('title' , 10);
         });
     }
 
