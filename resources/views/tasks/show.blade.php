696bc2d20ee980d68b4d0c0ea83be515fde3580b
marienaomi.iwata <marienaomi.iwata@rakuten.com>
2018-06-04 01:59:31 +0000

    bootstrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,14 +3,32 @@
 @section('content')
 
  <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+ 
+ <table>
+     <tr>
+         <th>id</th>
+         <td>{{ $task->status }}</td>
+     </tr>
+     
+     <tr>
+         <th>タスク</th>
+         <td>{{ $task->content }}</td>
+     </tr>
+     
+     <tr>
+         <th>ステータス</th>
+         <td>{{ $task->status }}</td>
+     </tr>
+ </table>
 
-    <p>{{ $task->content }}</p>
-    <p>{{ $task->status }}</p>
     
-    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
+    <br>
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-info']) !!}
+    <br>
+    <br>
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 <!-- Write content for each page here -->
 
